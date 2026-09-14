09d520be4a3a62c53505137a13c92f2d0e53f4b1
Gero Flucke <gero.flucke@xfel.eu>
2026-09-14 16:59:04 +0200

    Prepare step 1

--- src/karaboWorkshopPipelines/KaraboWorkshopPipelines.py
+++ src/karaboWorkshopPipelines/KaraboWorkshopPipelines.py
@@ -12,7 +12,7 @@
 # Karabo itself is licensed under the terms of the MPL 2.0 license.
 #############################################################################
 
-from karabo.middlelayer import Device, InputChannel, State
+from karabo.middlelayer import AccessMode, Device, InputChannel, State, UInt32
 
 from ._version import version as deviceVersion
 
